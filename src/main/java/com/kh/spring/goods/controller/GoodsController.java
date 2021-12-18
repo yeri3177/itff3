14c2c87aa900a9bb81b8d8cf9209e07000015c83
=yeri3177 <yeri3177@gmail.com>
2021-12-18 20:56:54 +0900

    goodsList 페이징 추가

--- src/main/java/com/kh/spring/goods/controller/GoodsController.java
+++ src/main/java/com/kh/spring/goods/controller/GoodsController.java
@@ -2,12 +2,16 @@ package com.kh.spring.goods.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kh.spring.common.HiSpringUtils;
 import com.kh.spring.goods.model.service.GoodsService;
 import com.kh.spring.goods.model.vo.Goods;
 
@@ -19,15 +23,33 @@ import lombok.extern.slf4j.Slf4j;
 public class GoodsController {
 	
 	@Autowired
-	private GoodsService goodService;
+	private GoodsService goodsService;
 	
 	@GetMapping("/goodsList.do")
-	public String goodsList(Model model) {
-	
-		List<Goods> list = goodService.selectGoodsList();
+	public String goodsList(@RequestParam(defaultValue = "1") int cPage, Model model, HttpServletRequest request) {
+		// 현재 페이지 번호 
+		log.debug("cPage = {}", cPage);
+		
+		// limit : 한페이지에 표시할 게시글 수 
+		// offset : 건너뛰어야 할 게시글 수
+		int limit = 8;
+		int offset = (cPage - 1) * limit;
+		
+		// 1. 상품 리스트 
+		List<Goods> list = goodsService.selectGoodsList(offset, limit);
+		log.debug("list = {}", list);
 		model.addAttribute("list", list);
 		
-		log.debug("list = {}", list);
+		// 2. 전체게시물수 
+		int totalContent = goodsService.selectGoodsTotalCount();
+		log.debug("totalContent = {}", totalContent);
+		model.addAttribute("totalContent", totalContent);
+		
+		// 3. pagebar
+		String url = request.getRequestURI(); // /spring/board/boardList.do
+		String pagebar = HiSpringUtils.getPagebar(cPage, limit, totalContent, url);
+		log.debug("pagebar = {}", pagebar);
+		model.addAttribute("pagebar", pagebar);
 		
 		return "goods/goodsList";
 	}
